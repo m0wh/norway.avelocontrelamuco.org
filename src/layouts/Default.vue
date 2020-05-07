042daa24fdc49be2d572114ad3b27e82fcfd95d6
m0wh <malo.dewiderspach@gmail.com>
2020-05-07 03:56:43 +0200

    hotfix

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -12,6 +12,9 @@ import { Vue, Component } from 'vue-property-decorator'
 import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
 import GridHelper from '../components/GridHelper.vue'
+import gsap from 'gsap'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
+import { CustomEase } from "gsap/CustomEase";
 
 @Component({
   components: {
@@ -20,7 +23,15 @@ import GridHelper from '../components/GridHelper.vue'
     GridHelper
   }
 })
-export default class DefaultLayout extends Vue {}
+export default class DefaultLayout extends Vue {
+  mounted () {
+    gsap.registerPlugin(ScrollToPlugin, CustomEase)
+
+    const cubicBezierRegex = /cubic-bezier\(([0-9]*?\.?[0-9]*?, ?[0-9]*?\.?[0-9]*?, ?[0-9]*?\.?[0-9]*?, ?[0-9]*?\.?[0-9]*?)\)/
+    const bezier: string = (window.getComputedStyle(document.documentElement).getPropertyValue('--main-easing').match(cubicBezierRegex) as string[])[1]
+    CustomEase.create('main', bezier)
+  }
+}
 </script>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -2,19 +2,8 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import gsap from 'gsap'
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
-import { CustomEase } from "gsap/CustomEase";
 
 export default function (Vue, { router, head, isClient }) {
-  gsap.registerPlugin(ScrollToPlugin, CustomEase)
-
-  const cubicBezierRegex = /cubic-bezier\(([0-9]*?\.?[0-9]*?, ?[0-9]*?\.?[0-9]*?, ?[0-9]*?\.?[0-9]*?, ?[0-9]*?\.?[0-9]*?)\)/
-  const bezier = window.getComputedStyle(document.documentElement)
-    .getPropertyValue('--main-easing')
-    .match(cubicBezierRegex)[1]
-  CustomEase.create('main', bezier)
-
   head.link.push({
     rel: 'stylesheet',
     href: '/fonts/suisse.css'
